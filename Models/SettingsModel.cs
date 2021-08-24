bb791dfc080e6c045d9e7ff96b76f25b933a2307
mpnogaj <mpnogaj@gmail.com>
2021-08-25 01:08:04 +0200

    Added theme field in settings

--- Models/SettingsModel.cs
+++ Models/SettingsModel.cs
@@ -1,4 +1,5 @@
 using Avalonia;
+using Avalonia.Themes.Fluent;
 using System;
 using WielkaSowa.ViewModels;
 
@@ -6,15 +7,24 @@ namespace WielkaSowa.Helpers
 {
 	public class SettingsModel : ViewModelBase
 	{
-		private bool _simpleUI = false;
+		private bool _simpleUI;
 		public bool SimpleUI 
 		{ 
 			get => _simpleUI;
 			set => SetProperty(ref _simpleUI, value);
 		}
-		public SettingsModel()
+
+		private bool _darkTheme;
+		public bool DarkTheme
+        {
+			get => _darkTheme;
+			set => SetProperty(ref _darkTheme, value);
+        }
+
+        public SettingsModel()
 		{
 			_simpleUI = false;
+			_darkTheme = true;
 		}
 
 		public void ApplySettings()
@@ -22,13 +32,15 @@ namespace WielkaSowa.Helpers
 			var res = Application.Current.Resources;
 			res["ComplexUI"] = !SimpleUI;
 			res["TransparencyHint"] = Converters.BoolToWindowTransparencyLevel.ToWindowTransparencyLevel(SimpleUI);
+			res["Theme"] = DarkTheme ? FluentThemeMode.Dark : FluentThemeMode.Light;
 		}
 
 		public static SettingsModel Clone(SettingsModel settingsModel)
         {
 			return new SettingsModel()
 			{
-				SimpleUI = settingsModel.SimpleUI
+				SimpleUI = settingsModel.SimpleUI,
+				DarkTheme = settingsModel.DarkTheme
 			};
         }
     }
